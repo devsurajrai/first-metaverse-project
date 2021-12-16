29d2f7d41e5234207d1a4d2836a3e204278b465a
Suraj Rai <surajrai@Surajs-MacBook-Pro.local>
2021-12-16 19:55:22 +0530

    styling

--- components/Login.js
+++ components/Login.js
@@ -8,13 +8,13 @@ function Login() {
       <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
         <Image
           className="object-cover rounded-full"
-          src="https://links.papareact.com/3pi"
+          src="https://bernardmarr.com/wp-content/uploads/2021/12/The-8-Most-Important-Metaverse-Companies.jpg"
           width={200}
           height={200}
         />
         <button
           onClick={authenticate}
-          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+          className="bg-blue-500 rounded-lg p-5 font-bold animate-pulse"
         >
           Login to the METAVERSE
         </button>
